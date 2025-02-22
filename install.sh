3cb34aeb7db11c7eb78efd5225e46cacc62377e6
Isaac <Isaachernandez174@gmail.com>
2025-02-21 16:30:59 -0800

    added install bash file

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the AstroChat repository
+git clone https://github.com/Cool-Coder174/AstroChat.git
+
+# Navigate into the AstroChat directory
+cd AstroChat
+
+# Install the AstroChat plugin
+echo "Installing AstroChat plugin..."
+# You can add plugin-specific installation commands here
+
+echo "AstroChat plugin installed successfully!"
